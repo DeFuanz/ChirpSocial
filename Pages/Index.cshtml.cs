fade1b7fdcacc5776484dae3c5fa4bccc3f56e3a
John DeFore <johndefore3@gmail.com>
2022-12-03 11:04:56 -0600

    Working on Login (landing page)

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using Chirp.Models;
 
 namespace ChirpSocial.Pages;
@@ -9,14 +10,32 @@ public class IndexModel : PageModel
 {
     private readonly ChirpDbContext _context; //Left off adding drop down to select profile
     private readonly ILogger<IndexModel> _logger;
+    public SelectList ProfileDropDown {get; set;} = default!;
+    public List<Profile> profiles {get; set;} = default!;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    [Required]
+    public Profile profile {get; set;} = default!;
+    public int SelectedProfileID {get; set;} 
+    public IndexModel(ChirpDbContext context, ILogger<IndexModel> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public void OnGet()
     {
+        profiles = _context.Profiles.ToList();
+        ProfileDropDown = new SelectList(profiles, "ProfileID", "ProfileUserName");
+    }
 
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        return RedirectToPage("./Index");
     }
 }
